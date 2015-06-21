0e21738dcb5741484fcb7d6f62378708182ae620
blackout314 <blackout_lair@yahoo.com>
2015-06-21 21:19:41 +0200

    remove unslider - code beauty

--- src/app/app.js
+++ src/app/app.js
@@ -85,11 +85,5 @@
 }(document, window));
 
 function doGall() {
-    $('.slider').unslider({
-        speed: 500,
-        delay: 3000,
-        keys: true,
-        dots: false,
-        fluid: false
-    });
+    // function call for make gall
 }
--- src/app/products/products.js
+++ src/app/products/products.js
@@ -16,38 +16,44 @@
                 });
         }])
 
-        .controller('ProductsCtrl', ['$scope', 'Product', '$routeParams', '$location', '$timeout', function ($scope, Product, $routeParams, $location, $timeout) {
-            $scope.products = {
-                //"sku1" : new Product( { "sku": "001", "name": "product1", "value":11 } ),
-                "sku2" : new Product( { "sku": "002", "name": "product2", "value":12 } )
-            };
+        .controller('ProductsCtrl', [
+            '$scope',
+            'Product',
+            '$routeParams',
+            '$location',
+            '$timeout',
+            function ($scope, Product, $routeParams, $location, $timeout) {
+                $scope.products = {
+                    //"sku1" : new Product({ "sku": "001", "name": "product1", "value": 11 }),
+                    "sku2" : new Product({ "sku": "002", "name": "product2", "value": 12 })
+                };
 
-            if ($routeParams.productId) {
-                // <- retrieve products data
-                console.log('ok');
-                // send to -> view
-                $scope.product = { "productID": $routeParams.productId };
-            }
+                if ($routeParams.productId) {
+                    // <- retrieve products data
+                    // console.log('ok');
+                    // send to -> view
+                    $scope.product = { "productID": $routeParams.productId };
+                }
 
-            /**
-             * @param id sku of item
-             */
-            $scope.addToCart = function (id) {
-                // -> add in the cart
-                //console.log('id add to cart'+id);
-            };
-            $scope.openItem = function (id) {
-                $location.path("/product/"+id);
-            };
+                /**
+                 * @param id sku of item
+                 */
+                $scope.addToCart = function (id) {
+                    // -> add in the cart
+                    //console.log('id add to cart'+id);
+                };
+                $scope.openItem = function (id) {
+                    $location.path("/product/" + id);
+                };
 
-            // TODO fix : prova barbara
-            $scope.$on('dataloaded', function () {
-                $timeout(function () {
-                    //console.log( 'L' + $('.cd-gallery').children('li').length );
-                    window.doGall();
-                }, 0, false);
-            });
+                // TODO fix : prova barbara
+                $scope.$on('dataloaded', function () {
+                    $timeout(function () {
+                        //console.log( 'L' + $('.cd-gallery').children('li').length );
+                        window.doGall();
+                    }, 0, false);
+                });
 
-            $scope.$emit('dataloaded');
-        }]);
+                $scope.$emit('dataloaded');
+            }]);
 }(document, window));
